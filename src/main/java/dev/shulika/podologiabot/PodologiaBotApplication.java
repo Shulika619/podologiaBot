1c7d74197530e6765827ab7d2cd5f0642d83a87b
Sergey Shulika <developer.shulika@gmail.com>
2023-04-13 08:37:44 +0300

    init commit

---
+++ src/main/java/dev/shulika/podologiabot/PodologiaBotApplication.java
@@ -0,0 +1,14 @@
+package dev.shulika.podologiabot;
+
+import dev.shulika.podologiabot.config.BotConfig;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PodologiaBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PodologiaBotApplication.class, args);
+    }
+
+}
